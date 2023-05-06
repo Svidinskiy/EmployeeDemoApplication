f14129b554f519fe74e5fd03f83ce6ca1e99f860
Svidinskiy <100801664+Svidinskiy@users.noreply.github.com>
2023-05-06 20:14:25 +0400

    Сделано ДЗ 2.5

---
+++ src/main/java/employeeexample/employeedemo/EmployeeController.java
@@ -0,0 +1,42 @@
+package employeeexample.employeedemo;
+
+import employeeexample.employeedemo.exceptions.EmployeeAlreadyAddedException;
+import employeeexample.employeedemo.exceptions.EmployeeNotFoundException;
+import employeeexample.employeedemo.exceptions.EmployeeStorageIsFullException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeServiceImpl employeeService;
+
+    public EmployeeController(EmployeeServiceImpl employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/add")
+    public Employee addEmployee(@RequestParam String firstName, @RequestParam String lastName) throws EmployeeStorageIsFullException, EmployeeAlreadyAddedException {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee removeEmployee(@RequestParam String firstName, @RequestParam String lastName) throws EmployeeNotFoundException {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee findEmployee(@RequestParam String firstName, @RequestParam String lastName) throws EmployeeNotFoundException {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/list")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
+
